8616fc6ec2e595f645b98c16c1fbe306d10675b6
Unknown <jcsouza.first@gmail.com>
2017-09-05 07:10:27 -0300

    Mudança completa em triangulo.java
    Modificando as constantes e trocando por enum. Diminuindo o código que verifica o tipo de triângulo substituindo os 4 métodos por um único. Utilização de switch no lugar de if else no teste do tipo de triângulo.

--- src/br/com/pm/lista1/Triangulo.java
+++ src/br/com/pm/lista1/Triangulo.java
@@ -8,117 +8,71 @@ import java.util.List;
 import java.util.Set;
 
 public class Triangulo{
-	private Set<Integer> ladosTriangulo;
+	private Set<Double> ladosTriangulo;
 	private String tipo;
-	private static final String NAO_EH_TRIANGULO = "Isto não é um triângulo"; 
-	private static final String EQUILATERO = "Equilátero";
-	private static final String ISOSCELES = "Isósceles";
-	private static final String ESCALENO = "Escaleno";
-	private static final String RETANGULO = "Retângulo";
-	
+	public enum tipoTriangulo{
+		NAO_EH_TRIANGULO("Isto não é um triângulo"), EQUILATERO("Equilátero"),
+		ISOSCELES("Isósceles"), ESCALENO("Escaleno");
+		
+		public String value;
+		tipoTriangulo(String value) {
+			this.value = value;
+		}
+		public String getValue() {
+			return value;
+		}
+	}
 	/**
-	 * 
-	 * @param lado1 inteiro maior que zero
-	 * @param lado2 inteiro maior que zero
-	 * @param lado3 inteiro maior que zero
-	 * 
-	 * constrói um triângulo a partir de três valore inteiros passados e 
-	 * testa para saber qual o seu tipo : Equilatero, Isosceles, Escaleno ou
-	 * Retangulo.
+	 * constrói um triângulo a partir de três valore reais passados e 
+	 * testa para saber qual o seu tipo : Equilatero, Isosceles, Escaleno.
 	 *  
+	 * @param lado1 número real maior que zero
+	 * @param lado2 número real maior que zero
+	 * @param lado3 número real maior que zero
+	 * @exception Throws IllegalArgumentException caso não seja um triangulo
 	 */
-	public Triangulo(int lado1, int lado2, int lado3) {
-		if((lado1 <= 0)||(lado2 <=0)||(lado3)<=0)
-			throw new IllegalArgumentException("lados do triangulo devem ser maiores do que zero");
+	public Triangulo(Double lado1, Double lado2, Double lado3) throws IllegalArgumentException{
 		if(ehTriangulo(lado1, lado2, lado3)) {
-			HashSet<Integer> ladosTriangulo = new HashSet<Integer>();
+			ladosTriangulo = new HashSet<Double>(3);
 			ladosTriangulo.add(lado1);
 			ladosTriangulo.add(lado2);
 			ladosTriangulo.add(lado3);
-			this.ladosTriangulo = ladosTriangulo;
-			if(equilatero())
-				tipo = EQUILATERO;
-			else {
-				if(isosceles())
-					tipo = ISOSCELES;
-				else {
-					if(escaleno() & trianguloRetangulo())
-						tipo = RETANGULO;
-					else
-						tipo = ESCALENO;
-				}	
-			}	
-		}else{
-			tipo = NAO_EH_TRIANGULO;
-		}
- 	}
+			String tipo = testaTipoTriangulo(ladosTriangulo); 
+			if(tipo.equals(tipoTriangulo.NAO_EH_TRIANGULO.getValue()))
+				throw new IllegalArgumentException(tipoTriangulo.NAO_EH_TRIANGULO.getValue());
+			else
+				this.tipo = tipo; 
+		}else
+			throw new IllegalArgumentException(tipoTriangulo.NAO_EH_TRIANGULO.getValue());
+
+	}
 	/**
 	 * Testa se os valores passados são um triangulo testanto se
 	 * a soma de dois lados é maior do que o tamanho do terceiro. 
 	 * @return boolean indicando se é triangulo ou não.
 	 */
-	private boolean ehTriangulo(int lado1, int lado2, int lado3){
+	public boolean ehTriangulo(Double lado1, Double lado2, Double lado3){
 		if(((lado1 + lado2) > lado3)&&((lado1 + lado3) > lado2)&&((lado2 + lado3) > lado1))
 			return true;
 		else 
 			return false;
 	}
 	/**
-	 * Utilizando HashSet testa se o triângulo é equilátero.
-	 * Se os valores passados pro construtor forem todos iguais,
-	 * o método size de HashSet retornará 1, indicando um triângulo
-	 * equilátero. 
-	 * @return boolean indicando se é equilatero ou não 
+	 * Testa o tipo de um triangulo dada a quantidade de valores no hashset passado
+	 * @param ladosTriangulo hashset contendo 1 a 3 valores
+	 * @return tipo do triangulo: Equilatero, Isosceles ou Escaleno
+	 * @exception Throws IllegalArgumentException caso o HashSet tenha 0 ou mais de 3 
+	 * valores
 	 */
-	private boolean equilatero(){
-		if(ladosTriangulo.size() == 1)
-			return true;
-		else 
-			return false;
-	}
-	/**
-	 * Utilizando HashSet testa se o triângulo é isosceles
-	 * Se os valores passados pro construtor tiverem dois 
-	 * iguais, o método size de HashSet retornará 2, indicando um 
-	 * triângulo isósceles
-	 * @return boolean indicando se é isósceles ou não
-	 */ 
-	private boolean isosceles(){
-		if(ladosTriangulo.size() == 2)
-			return true;
-		else
-			return false;
-	}
-	/**
-	 * Utilizando HashSet testa se o triângulo é escaleno
-	 * Se os valores passados pro construtor não tiver nenhum valor 
-	 * igual, o método size de HashSet retornará 3, indicando um 
-	 * triângulo escaleno.
-	 * @return boolean indicando se é escaleno ou não
-	 */ 
-	private boolean escaleno() {
-		if(ladosTriangulo.size() == 3)
-			return true;
-		else
-			return false;
-	}
-	/**
-	 * Testa se o triângulo é retangulo isando pitágoras 
-	 * e testando antes se ele é escaleno.
-	 * @return boolean indicando se é retângulo ou não
-	 */
-	private boolean trianguloRetangulo(){
-		if(escaleno()) {
-			List<Integer> ladosOrdenados = new ArrayList<Integer>(ladosTriangulo);
-			Collections.sort(ladosOrdenados);
-			int catetoA = ladosOrdenados.get(0);
-			int catetoB = ladosOrdenados.get(1);
-			int hipotenusa = ladosOrdenados.get(2);
-			if(((catetoA*catetoA) + (catetoB*catetoB)) == (hipotenusa*hipotenusa)) {
-				return true;
-			}
+	public String testaTipoTriangulo(Set<Double> ladosTriangulo) {
+		if((ladosTriangulo.size() == 0)||(ladosTriangulo.size() > 3))
+			throw new IllegalArgumentException(tipoTriangulo.NAO_EH_TRIANGULO.getValue());
+		switch (ladosTriangulo.size()) {
+			case 1: return tipoTriangulo.EQUILATERO.getValue();
+			case 2: return tipoTriangulo.ISOSCELES.getValue();
+			case 3: return tipoTriangulo.ESCALENO.getValue();
 		}
-		return false;
+		return tipoTriangulo.NAO_EH_TRIANGULO.getValue();
 	}
 	public String getTipo() {
 		return tipo;
